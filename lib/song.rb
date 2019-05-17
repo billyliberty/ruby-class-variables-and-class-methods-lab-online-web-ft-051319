1ceb5c046e6402e64027a103cb876ab5d0c85776
Joshua Crum <joshua.crum@gmail.com>
2019-05-17 19:40:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,39 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@song_number = 0
+  @@genres = []
+  @@artists = []
 
-  def initialize
+  def initialize(name, artist, genre)
     @@song_number += 1
+    @name = name
+    @artist = artist
+    @@genres << genres
+    @@artists << artist
   end
 
   def self.count
     @@song_number
   end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self genre_type
+    if genre_type[genre]
+      genre_type[genre] += 1
+    else
+      genre_type[genre] = 1
+    end
+  end
+  genre_type
+end
+  
+
+  
 end
