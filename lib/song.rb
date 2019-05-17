2383801f7c62f9059bc67cebcc2f550b9ee588d7
Joshua Crum <joshua.crum@gmail.com>
2019-05-17 19:52:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,12 @@ class Song
   @@artists = []
 
   def initialize(name, artist, genre)
-    @@song_count += 1
     @name = name
     @artist = artist
     @genre = genre
-    @@genres << genre
-    @@artists << artist
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
   end
 
   def self.count
