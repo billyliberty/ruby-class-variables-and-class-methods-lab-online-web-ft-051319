c8d8f0e14fa819f833074f2b3412b01bfc490546
Joshua Crum <joshua.crum@gmail.com>
2019-05-17 19:49:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,16 +26,16 @@ class Song
     @@artists.uniq
   end
 
-  def self.genre_type
-    genre_type = {}
+  def self.genre_count
+    genre_count = {}
     @@genres.each do |genre|
-    if genre_type[genre]
-      genre_type[genre] += 1
+    if genre_count[genre]
+      genre_count[genre] += 1
     else
-      genre_type[genre] = 1
+      genre_count[genre] = 1
     end
   end
-  genre_type
+  genre_count 
 end
 
 def self.artist_count
